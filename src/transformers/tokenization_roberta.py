ca6aa9bada48ec3ac9e670c3f60756e022738d53
s181646 <andrew.lyon@UTSouthwestern.edu>
2020-02-20 22:24:36 -0600

    Add AATransformer class

--- src/transformers/tokenization_roberta.py
+++ src/transformers/tokenization_roberta.py
@@ -18,7 +18,7 @@
 import logging
 
 from tokenizers.processors import RobertaProcessing
-
+from .tokenization_utils import PreTrainedTokenizer
 from .tokenization_gpt2 import GPT2Tokenizer, GPT2TokenizerFast
 
 
@@ -223,3 +223,158 @@ class RobertaTokenizerFast(GPT2TokenizerFast):
             return output
 
         return output + [self.eos_token_id] + token_ids_1 + [self.eos_token_id]
+
+
+class AATokenizer(PreTrainedTokenizer):
+    def vocab_size(self):
+        pass
+
+    def __init__(
+            self,
+            errors="replace",
+            bos_token="<s>",
+            eos_token="</s>",
+            sep_token="</s>",
+            cls_token="<s>",
+            unk_token="<unk>",
+            pad_token="<pad>",
+            mask_token="<mask>",
+            valid_aas=tuple(res for res in 'MRHKDESTNQCUGPAVIFYWLO'),
+            ** kwargs
+    ):
+        super().__init__(
+            errors=errors,
+            bos_token=bos_token,
+            eos_token=eos_token,
+            unk_token=unk_token,
+            sep_token=sep_token,
+            cls_token=cls_token,
+            pad_token=pad_token,
+            mask_token=mask_token,
+            **kwargs,
+        )
+        self.max_len_single_sentence = self.max_len - 2  # take into account special tokens
+        self.max_len_sentences_pair = self.max_len - 4  # take into account special tokens
+        self.valid_aas = valid_aas
+
+    def build_inputs_with_special_tokens(self, token_ids_0, token_ids_1=None):
+        """
+        Build model inputs from a sequence or a pair of sequence for sequence classification tasks
+        by concatenating and adding special tokens.
+        A RoBERTa sequence has the following format:
+            single sequence: <s> X </s>
+            pair of sequences: <s> A </s></s> B </s>
+        """
+        if token_ids_1 is None:
+            return [self.cls_token_id] + token_ids_0 + [self.sep_token_id]
+        cls = [self.cls_token_id]
+        sep = [self.sep_token_id]
+        return cls + token_ids_0 + sep + sep + token_ids_1 + sep
+
+    def get_special_tokens_mask(self, token_ids_0, token_ids_1=None, already_has_special_tokens=False):
+        """
+        Retrieves sequence ids from a token list that has no special tokens added. This method is called when adding
+        special tokens using the tokenizer ``prepare_for_model`` or ``encode_plus`` methods.
+        Args:
+            token_ids_0: list of ids (must not contain special tokens)
+            token_ids_1: Optional list of ids (must not contain special tokens), necessary when fetching sequence ids
+                for sequence pairs
+            already_has_special_tokens: (default False) Set to True if the token list is already formated with
+                special tokens for the model
+        Returns:
+            A list of integers in the range [0, 1]: 1 for a special token, 0 for a sequence token.
+        """
+        if already_has_special_tokens:
+            if token_ids_1 is not None:
+                raise ValueError(
+                    "You should not supply a second sequence if the provided sequence of "
+                    "ids is already formated with special tokens for the model."
+                )
+            return list(map(lambda x: 1 if x in [self.sep_token_id, self.cls_token_id] else 0, token_ids_0))
+
+        if token_ids_1 is None:
+            return [1] + ([0] * len(token_ids_0)) + [1]
+        return [1] + ([0] * len(token_ids_0)) + [1, 1] + ([0] * len(token_ids_1)) + [1]
+
+    def _tokenize(self, text, **kwargs):
+        # If the sequence begins with M start codon, substitute it for 'start' token
+        if text[0] == 'M':
+            return ['start'] + [res if res in self.valid_aas else self.unk_token for res in text[1:]]
+        else:
+            # Otherwise the sequence does not have a start codon (e.g. it's post-translationally clipped off)
+            return [res if res in self.valid_aas else self.unk_token for res in text]
+
+    def _convert_token_to_id_with_added_voc(self, token):
+        """Overrides method from base class. Called by convert_tokens_to_ids()"""
+        if token is None:
+            return None
+        return self._convert_token_to_id(token)
+
+    def _convert_token_to_id(self, token):
+        """Implements abstract method"""
+        aa_to_id = {'M': 4,
+                    'R': 5,
+                    'H': 6,
+                    'K': 7,
+                    'D': 8,
+                    'E': 9,
+                    'S': 10,
+                    'T': 11,
+                    'N': 12,
+                    'Q': 13,
+                    'C': 14,
+                    'U': 15,
+                    'G': 16,
+                    'P': 17,
+                    'A': 18,
+                    'V': 19,
+                    'I': 20,
+                    'F': 21,
+                    'Y': 22,
+                    'W': 23,
+                    'L': 24,
+                    'O': 25,
+                    'start': 26,
+                    '<unk>': 3,
+                    '<s>': 0,
+                    '</s>': 2}
+        return aa_to_id[token]
+
+    def _convert_id_to_token(self, index):
+        id_to_aa = {4: 'M',
+                    5: 'R',
+                    6: 'H',
+                    7: 'K',
+                    8: 'D',
+                    9: 'E',
+                    10: 'S',
+                    11: 'T',
+                    12: 'N',
+                    13: 'Q',
+                    14: 'C',
+                    15: 'U',
+                    16: 'G',
+                    17: 'P',
+                    18: 'A',
+                    19: 'V',
+                    20: 'I',
+                    21: 'F',
+                    22: 'Y',
+                    23: 'W',
+                    24: 'L',
+                    25: 'O',
+                    26: 'start',
+                    3: '<unk>',
+                    0: '<s>',
+                    2: '</s>'}
+        return id_to_aa[index]
+
+    def convert_tokens_to_string(self, tokens):
+        """Overriding base method as we don't want amino acids separated by spaces"""
+        return "".join(self.convert_ids_to_tokens(tokens))
+
+    def save_vocabulary(self, save_directory):
+        voc = '\n'.join(['start'] + [res for res in self.valid_aas])
+        with open(save_directory, 'w') as oh:
+            oh.write(voc)
+
